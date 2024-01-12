97efad2c94e7b232251d86da7b35d096ba16eaa5
iffyloop <35245220+iffyloop@users.noreply.github.com>
2024-01-11 22:06:31 -0600

    Make it compile on macOS

--- src/m3d.cpp
+++ src/m3d.cpp
@@ -3,7 +3,7 @@
 #define M3D_IMPLEMENTATION
 #include "m3d.h"
 #ifndef NDEBUG
-#define GLAD_WRAPPER_ENABLED
+// #define GLAD_WRAPPER_ENABLED // FIXME: We have disabled error checking for now, so we can keep compiling on macOS until we're able to fix the error
 #endif
 #define GLAD_WRAPPER_IMPLEMENTATION
 #include "glad/glad.h"
