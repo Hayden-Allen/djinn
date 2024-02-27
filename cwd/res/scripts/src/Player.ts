533e792379840edc20ba7d251e79ccf0fcd0d396
Hayden Allen <hsa190003@utdallas.edu>
2024-02-26 23:05:11 -0600

    initial commit

--- cwd/res/scripts/src/Player.ts
+++ cwd/res/scripts/src/Player.ts
@@ -6,51 +6,19 @@ const { Asset, Event, Scene, Input, ImGui, Sound } = djinn
 
 export default class Player extends Entity {
     private camera?: Camera
-    private idMainMesh: MeshID
-    private idMainInstance: MeshInstanceID
-    private idMainShader: ShaderID
-    private idWingMesh: MeshID
-    private idWingInstance: MeshInstanceID
-    private idWingShader: ShaderID
-    private idWingFrontTexture: TextureID
-    private idWingBackTexture: TextureID
-    private wingBoneNames: string[] = [
-        "mixamorig:LeftArm",
-        "mixamorig:LeftForeArm",
-        "mixamorig:LeftHand",
-        "mixamorig:LeftUpLeg",
-        "mixamorig:LeftLeg",
-        "mixamorig:Spine1",
-    ]
-    private wingUVs: number[][] = [
-        [1 / 3, 1],
-        [2 / 3, 1],
-        [1, 1],
-        [1 / 4, 1 / 3],
-        [1 / 4, 0],
-        [0, 2 / 3],
-    ]
     private idHitbox: PhysicsID
-    private isJumping: boolean = false
     private camAngleX: number = 0
     private camAngleY: number = 0
-    private meshScale: number = 0.1
-    private hitboxHeight: number = 1
-    private hitboxRadius: number = 0.2
+    private hitboxHeight: number = .1
+    private hitboxRadius: number = .1
+    private worldPos: number[] = [0, 0, 0]
 
-    // private worldPos: number[] = [-36, 103, -39] // top of tower
-    // private worldPos: number[] = [-29, 30, 39] // ground
-    // private worldPos: number[] = [-14, 90, 153] // gate
-    private worldPos: number[] = [-28, 126, 125] // platform
-    private moveDir: number[] = [0, 0, 0]
-    private velY: number = 0
-    private velYMin: number = -35
-    private velYMax: number = 25
-    private gravity: number = 75
     private canJump: boolean = false
-    private lastGravity: number = 0
-    private lastTime: number = 0
-    private lastDelta: number[] = [0, 0, 0]
+    private velY: number = 0
+    private velYMin: number = -100
+    private velYMax: number = 20
+    private gravity: number = 65
+    private runSpeed: number = 1.5
 
     __init(cam: Camera) {
         if (cam) {
@@ -59,44 +27,6 @@ export default class Player extends Entity {
     }
     __load() {
         ImGui.setName(this.id, "Player")
-        // character
-        {
-            this.idMainMesh = Asset.Mesh.loadAnimated("xbot.m3d")
-            this.idMainShader = Asset.Shader.load(
-                "animated.vert",
-                "animated.frag"
-            )
-            this.idMainInstance = Scene.MeshInstance.create(
-                this.idMainMesh,
-                this.idMainShader
-            )
-            Scene.MeshInstance.setAction(this.idMainInstance, "idle_Armature")
-        }
-        // procedural wing
-        {
-            this.idWingFrontTexture = Asset.Texture.load("wing_front.png")
-            this.idWingBackTexture = Asset.Texture.load("wing_back.png")
-            this.idWingMesh = Asset.Mesh.create(
-                this.wingBoneNames.length * 2,
-                [3, 3, 2, 1],
-                24,
-                [this.idWingFrontTexture, this.idWingBackTexture]
-            )
-            this.idWingShader = Asset.Shader.load("wing.vert", "wing.frag")
-            Asset.Shader.setUniforms(this.idWingShader, {
-                u_texture_front: 0,
-                u_texture_back: 1,
-            })
-            this.idWingInstance = Scene.MeshInstance.create(
-                this.idWingMesh,
-                this.idWingShader
-            )
-            const idx = [
-                5, 1, 0, 5, 2, 1, 3, 2, 5, 4, 2, 3, 7, 11, 6, 8, 11, 7, 8, 9,
-                11, 8, 10, 9,
-            ]
-            Asset.Mesh.updateIndices(this.idWingMesh, idx)
-        }
         // hitbox
         {
             this.idHitbox = Scene.Physics.createCapsuleY(
@@ -112,77 +42,17 @@ export default class Player extends Entity {
         }
         // scene graph
         {
-            Scene.setParent(this.idMainInstance, this.idHitbox)
-            Scene.setRotY(this.idMainInstance, 180)
-            Scene.setPosY(
-                this.idMainInstance,
-                -this.hitboxHeight / 2 - this.hitboxRadius
-            )
-
-            Scene.setParent(this.idWingInstance, this.idMainInstance)
-
             if (this.camera) {
                 const idCam = this.camera!.getId()
                 Scene.setParent(idCam, this.idHitbox)
-                Scene.setPosZ(idCam, 2)
-                Scene.setPosY(idCam, 1)
+                Scene.setPosY(idCam, .025)
             }
         }
     }
     __unload() {
-        Asset.Texture.destroyAll([
-            this.idWingFrontTexture,
-            this.idWingBackTexture,
-        ])
-        Scene.MeshInstance.destroy(this.idMainInstance)
-        Asset.Mesh.destroy(this.idMainMesh)
-        Asset.Shader.destroy(this.idMainShader)
-        Scene.MeshInstance.destroy(this.idWingInstance)
-        Asset.Mesh.destroy(this.idWingMesh)
-        Asset.Shader.destroy(this.idWingShader)
         Scene.Physics.destroy(this.idHitbox)
     }
     __main(dt: number, time: number) {
-        this.lastTime = time
-        // wing mesh update
-        {
-            let frontWingVerts: number[] = []
-            let backWingVerts: number[] = []
-            for (var i = 0; i < this.wingBoneNames.length; i++) {
-                // pos
-                const pos = Scene.MeshInstance.getBonePos(
-                    this.idMainInstance,
-                    this.wingBoneNames[i]
-                )
-                if (this.isJumping) {
-                    const factor = 0.01
-                    pos[0] += Math.random() * factor - factor / 2
-                    pos[1] += Math.random() * factor - factor / 2
-                    pos[2] += Math.random() * factor - factor / 2
-                }
-                frontWingVerts = frontWingVerts.concat([...pos])
-                backWingVerts = backWingVerts.concat(pos)
-                // normal
-                const basis = Scene.MeshInstance.getBoneBasisZ(
-                    this.idMainInstance,
-                    this.wingBoneNames[i]
-                )
-                basis[1] *= -1
-                frontWingVerts = frontWingVerts.concat(basis)
-                const minusBasis = [-basis[0], -basis[1], -basis[2]]
-                backWingVerts = backWingVerts.concat(minusBasis)
-                // uv
-                frontWingVerts = frontWingVerts.concat(this.wingUVs[i])
-                backWingVerts = backWingVerts.concat(this.wingUVs[i])
-                // tex index
-                frontWingVerts.push(0)
-                backWingVerts.push(1)
-            }
-            Asset.Mesh.updateVertices(
-                this.idWingMesh,
-                frontWingVerts.concat(backWingVerts)
-            )
-        }
         // camera
         {
             this.camAngleY -= dt * 180 * Input.rightX()
@@ -198,46 +68,22 @@ export default class Player extends Entity {
         // movement
         {
             const boost = Input.getKey(Input.KEY_LEFT_CONTROL) ? 5 : 1
-            const x = boost * 25 * Input.leftX()
-            const z = boost * 25 * Input.leftY()
-            this.lastGravity = this.gravity * dt
+            const x = boost * this.runSpeed * Input.leftX()
+            const z = boost * this.runSpeed * Input.leftY()
             let newVelY = this.velY - boost * this.gravity * dt
-            // if (this.canJump) {
-            if (Input.getKey(Input.KEY_SPACE)) {
-                this.canJump = false
-                newVelY += boost * this.velYMax
-                Scene.unsetParentKeepTransform(this.idHitbox)
+            if (this.canJump) {
+                if (Input.getKey(Input.KEY_SPACE)) {
+                    this.canJump = false
+                    newVelY += this.velYMax
+                    Scene.unsetParentKeepTransform(this.idHitbox)
+                }
             }
-            // }
             this.velY = Math.min(
-                boost * this.velYMax,
-                Math.max(boost * this.velYMin, newVelY)
+                this.velYMax,
+                Math.max(this.velYMin, newVelY)
             )
             const dir = [x, this.velY, z]
             Scene.Physics.collideNSlide(this.idHitbox, dir, dt, { x: 1, z: 1, y: 1 })
-            this.moveDir = dir
-
-            let actionSet = false
-            if (dir[0] != 0 || dir[2] != 0) {
-                Scene.MeshInstance.setAction(
-                    this.idMainInstance,
-                    "run_Armature"
-                )
-                actionSet = true
-            }
-            if (Input.getKey(Input.KEY_SPACE)) {
-                Scene.MeshInstance.setAction(this.idMainInstance, "bind")
-                actionSet = true
-                this.isJumping = true
-            } else {
-                this.isJumping = false
-            }
-            if (!actionSet) {
-                Scene.MeshInstance.setAction(
-                    this.idMainInstance,
-                    "idle_Armature"
-                )
-            }
         }
         Scene.Entity.requestImGui(this.id)
 
@@ -253,26 +99,14 @@ export default class Player extends Entity {
             //         printf(`${i} (${this.lastDelta[i]} => ${newDelta[i]}) ${dt}ms`)
             //     }
             // this.lastDelta = newDelta
-            printf(idParent, Scene.getUserPointer(idParent).vel)
+            // printf(idParent, Scene.getUserPointer(idParent).vel)
         }
     }
     __collide_phorm(id: PhormID, normalWorld: number[]) {
-        const isTrigger = Scene.Tag.has(id, "trigger")
-        if (normalWorld[1] >= 0.9 && !isTrigger) {
-            if (!this.canJump) {
-                // Event.dispatch("player_can_jump", "hello, world")
-            }
+        if (normalWorld[1] >= 0.9) {
             this.canJump = true
             this.velY = 0
-            if (Scene.Tag.has(id, "switch")) {
-                // Event.dispatch("player_hit_switch", Scene.Xport.getName(id))
-            }
             Scene.setParentKeepTransform(this.idHitbox, id)
-        }
-        if (isTrigger) {
-            const name = Scene.Xport.getName(id)
-            const tagz = Scene.Tag.get(id)
-            // printf(`Collide with: '${name}' [${tagz}]`)
         }
     }
     __no_collide() {
@@ -291,34 +125,13 @@ export default class Player extends Entity {
         Sound.setListenerWorldDir(dir)
 
         // update shaders AFTER CAMERA TRANSFORM IS DONE BEING MODIFIED
-        Asset.Shader.setCameraUniforms(this.idMainShader, this.camera!.getId())
-        // Asset.Shader.setCameraUniforms(this.idWingShader, this.camera!.getId())
     }
     __imgui() {
-        ImGui.text("Pos: " + Scene.getPosWorld(this.idHitbox))
-        const ivel = Scene.Physics.getVelocity(this.idHitbox).map((v) =>
-            Math.round(v)
-        )
-        ImGui.text("Vel: " + ivel)
-        ImGui.text(
-            "Speed: " + Math.round(Scene.Physics.getSpeed(this.idHitbox))
-        )
-        ImGui.text("Cam Theta: " + Math.round(this.camAngleY))
-        let moveWorld = Scene.Physics.getVelocityWorld(this.idHitbox)
-        const length = Math.sqrt(
-            moveWorld[0] ** 2 + moveWorld[1] ** 2 + moveWorld[2] ** 2
-        )
-        moveWorld = [
-            moveWorld[0] / length,
-            moveWorld[1] / length,
-            moveWorld[2] / length,
-        ]
-        ImGui.text("Move World: " + moveWorld)
-        ImGui.separator()
+        const mag = ImGui.sliderFloat("Jump", this.velYMax, 0, 100);
+        this.velYMax = mag;
+        this.velYMin = -mag;
+        this.gravity = ImGui.sliderFloat("Gravity", this.gravity, 0, 100);
 
-        ImGui.text("VelY: " + this.velY)
-        this.velYMin = ImGui.sliderFloat("VelYMin: ", this.velYMin, -50, 0)
-        this.velYMax = ImGui.sliderFloat("VelYMax: ", this.velYMax, 0, 50)
-        this.gravity = ImGui.sliderFloat("Gravity: ", this.gravity, 0, 150)
+        this.runSpeed = ImGui.sliderFloat("Run Speed", this.runSpeed, 0, 25);
     }
 }
--- cwd/res/scripts/src/main.ts
+++ cwd/res/scripts/src/main.ts
@@ -1,125 +1,30 @@
 import "./lib/djinn.d"
 import Camera from "./lib/Camera"
 import Entity from "./lib/Entity"
-import Color from "./lib/Color"
-import TestEntity from "./TestEntity"
-import GroundEntity from "./GroundEntity"
 import Xport from "./lib/Xport"
 import Player from "./Player"
 
-const { Asset, Event, Render, Nanovg, Scene, ImGui, Input, Sound } = djinn
+const { Asset, Render, Scene, Input } = djinn
 
 export default class MainEntity extends Entity {
     private camera?: Camera
-    private color: Color = new Color(0, 0, 0, 1)
-    private entities: TestEntity[] = []
-    private idShader: ShaderID
-    private idTexture: TextureID
-    private idMesh: MeshID
-    private idSoundSource: SoundID
-    private idSoundEmitter: SoundEmitterID
-    private needsPlayAudio: boolean = true
-
-    private idStaticMesh: MeshID
-    private idStaticInstance: MeshInstanceID
-    private idStaticShader: ShaderID
-
-    private idAnimatedMesh: MeshID
-    private idAnimatedInstances: MeshInstanceID[] = []
-    private idAnimatedShader: ShaderID
-
-    private frame: number = 0
-    private nextAnimated: number = 0
-
-    private idPhysics: PhysicsID
-    private ground?: GroundEntity
-
     private idPhormShader: ShaderID
     private idPhormAlphaShader: ShaderID
     private idPhormWaterShader: ShaderID
     private xport?: Xport
-
     private player?: Player
 
-    handle_player_can_jump(event: string, payload: any, user: number) {
-        printf(event, payload, user)
-    }
-    handle_player_can_jump2(event: string, payload: any) {
-        printf("JUMP 2")
-    }
-    handle_player_hit_switch(event: string, payload: any, user: any) {
-        const switchName = payload as string
-        const switchNum = switchName.substring(6)
-        const doorName = "door" + switchNum
-        const idDoor = user.xport.getPhormByName(doorName)
-        Scene.addPosYWorld(idDoor, -0.005)
-    }
     __init() {
         Asset.setDefaultTextureOptions({
             minFilter: Render.GL_LINEAR_MIPMAP_LINEAR,
             magFilter: Render.GL_LINEAR,
         })
-        Event.subscribe("player_can_jump", this.handle_player_can_jump, 0)
-        Event.subscribe("player_can_jump", this.handle_player_can_jump, 1)
-        Event.subscribe("player_can_jump", this.handle_player_can_jump2, 1)
-        Event.subscribe(
-            "player_hit_switch",
-            this.handle_player_hit_switch,
-            this
-        )
-
+        
         this.camera = Scene.Camera.load("lib/Camera.js") as Camera
         const ar = Render.getAspectRatio()
         Scene.Camera.configure(this.camera.getId(), 108 / ar, ar, 0.1, 1000)
 
-        this.idShader = Asset.Shader.load("custom.vert", "custom.frag")
-        this.idTexture = Asset.Texture.load("test.bmp", {
-            minFilter: Render.GL_NEAREST,
-            magFilter: Render.GL_LINEAR,
-        })
-        Asset.Shader.setUniforms(this.idShader, {
-            u_texture: 0,
-        })
-        this.idMesh = Asset.Mesh.create(4, [2, 2], 6, [this.idTexture])
-
-        this.idSoundSource = Asset.Sound.load("white_out.mp3")
-        this.idSoundEmitter = Scene.SoundEmitter.create(this.idSoundSource)
-        Scene.setPos(this.idSoundEmitter, [-36, 103, -39])
-        Scene.SoundEmitter.setSpatializationEnabled(this.idSoundEmitter, true)
-        // Scene.SoundEmitter.setVolume(this.idSoundEmitter, 10)
-        Scene.SoundEmitter.setLooping(this.idSoundEmitter, true)
-        Scene.SoundEmitter.setRolloff(this.idSoundEmitter, 2)
-        this.needsPlayAudio = true
-
-        this.idStaticMesh = Asset.Mesh.loadStatic("cube.m3d")
-        this.idStaticShader = Asset.Shader.load("static.vert", "static.frag")
-        this.idStaticInstance = Scene.MeshInstance.create(
-            this.idStaticMesh,
-            this.idStaticShader
-        )
-
-        this.idAnimatedMesh = Asset.Mesh.loadAnimated("xbot.m3d")
-        this.idAnimatedShader = Asset.Shader.load(
-            "animated.vert",
-            "animated.frag"
-        )
-        for (var i = 0; i < 10; i++) {
-            this.idAnimatedInstances.push(
-                Scene.MeshInstance.create(
-                    this.idAnimatedMesh,
-                    this.idAnimatedShader
-                )
-            )
-            Scene.setPosX(this.idAnimatedInstances[i], -5 + i)
-            Scene.setPosZ(this.idAnimatedInstances[i], -3)
-        }
-        Scene.setPos(this.idAnimatedInstances[0], [-36, 103, -39])
-
-        this.idPhysics = Scene.Physics.createBox(1, [0, 50, 0], [1, 1, 1])
-
-        this.ground = Scene.Entity.load("GroundEntity.js") as GroundEntity
-        this.ground?.bind(this.camera)
-
+        this.xport = new Xport("01.xport")
         this.idPhormShader = Asset.Shader.load("phorm.vert", "phorm.frag")
         this.idPhormAlphaShader = Asset.Shader.load(
             "phorm.vert",
@@ -128,111 +33,31 @@ export default class MainEntity extends Entity {
         this.idPhormWaterShader = Asset.Shader.load(
             "phorm_water.vert",
             "phorm_water.frag"
-        )
-        this.xport = new Xport("castle4.xport")
+        )       
         for (var i = 0; i < this.xport.idPhorms.length; i++) {
             const id = this.xport.idPhorms[i]
-            if (Scene.Tag.has(id, "water")) {
-                Scene.Phorm.setShaders(id, this.idPhormWaterShader)
-                Scene.Phorm.setLayer(id, 1)
-            } else Scene.Phorm.setShaders(id, this.idPhormShader)
+            Scene.Phorm.setShaders(id, this.idPhormShader)
             Scene.Phorm.setAlphaShaders(id, this.idPhormAlphaShader)
         }
-        const platforms = this.xport!.getPhormsByTag("platform")
-        for (const platform of platforms!) {
-            printf(platform)
-            Scene.setUserPointer(platform, { vel: [0, 0, -5] })
-        }
-
+        
         this.player = Scene.Entity.load("Player.js", this.camera!) as Player
     }
     __destroy() {
-        Scene.MeshInstance.destroy(this.idStaticInstance)
-        Asset.Mesh.destroy(this.idStaticMesh)
-        Asset.Shader.destroy(this.idStaticShader)
-        Scene.MeshInstance.destroyAll(this.idAnimatedInstances)
-        Asset.Mesh.destroy(this.idAnimatedMesh)
-        Asset.Shader.destroy(this.idAnimatedShader)
-
-        Scene.SoundEmitter.stop(this.idSoundEmitter)
-        Scene.SoundEmitter.destroy(this.idSoundEmitter)
-        Asset.Sound.destroy(this.idSoundSource)
-        Asset.Mesh.destroy(this.idMesh)
-        Asset.Shader.destroy(this.idShader)
-        Asset.Texture.destroy(this.idTexture)
-
-        Scene.Physics.destroy(this.idPhysics)
-
         this.xport!.destroy()
         Asset.Shader.destroy(this.idPhormShader)
         Asset.Shader.destroy(this.idPhormAlphaShader)
         Asset.Shader.destroy(this.idPhormWaterShader)
     }
-    __load() {
-        this.color.set(0, 1, 1, 0.5)
-        // for (var i = 0; i < 100; i++) {
-        //   let e = Scene.Entity.load("TestEntity.js") as TestEntity
-        //   e.bind(this.idMesh, this.idShader)
-        //   this.entities.push(e)
-        // }
-    }
-    __unload() {
-        for (var i = 0; i < this.entities.length; i++)
-            Scene.Entity.destroy(this.entities[i].getId())
-        this.entities = []
-    }
-    private unsubscribed: boolean = false
     __main(dt: number, time: number) {
-        this.frame++
-        if (
-            this.nextAnimated < this.idAnimatedInstances.length &&
-            this.frame >= this.nextAnimated * 100
-        ) {
-            Scene.MeshInstance.setAction(
-                this.idAnimatedInstances[this.nextAnimated],
-                "run_Armature",
-                // 1 + this.nextAnimated / 10
-                0.1
-            )
-            this.nextAnimated++
-        }
-
-        if (this.needsPlayAudio) {
-            // Scene.SoundEmitter.start(this.idSoundEmitter)
-            this.needsPlayAudio = false
-        }
-
         if (Input.getKey(Input.KEY_ENTER)) {
             Render.setDebugDrawEnabled(true)
         }
         if (Input.getKey(Input.KEY_BACKSPACE)) {
             Render.setDebugDrawEnabled(false)
         }
-        if (Input.getKey(Input.KEY_0) && !this.unsubscribed) {
-            Event.unsubscribe("player_can_jump", this.handle_player_can_jump)
-            this.unsubscribed = true
-        }
-        if (Input.getKey(Input.KEY_1))
-            Scene.SoundEmitter.setFade(this.idSoundEmitter, -1, 0, 1000)
-        else if (Input.getKey(Input.KEY_2))
-            Scene.SoundEmitter.setFade(this.idSoundEmitter, -1, 1, 1000)
-
-        const platforms = this.xport!.getPhormsByTag("platform")
-        for (const platform of platforms!) {
-            Scene.addPosZWorld(platform, -5 * dt)
-        }
     }
     __draw() {
         this.xport!.skybox!.draw(this.camera!.getId())
-        Asset.Shader.setCameraUniforms(this.idShader, this.camera!.getId())
-        Asset.Shader.setCameraUniforms(
-            this.idStaticShader,
-            this.camera!.getId()
-        )
-        Asset.Shader.setCameraUniforms(
-            this.idAnimatedShader,
-            this.camera!.getId()
-        )
         Asset.Shader.setCameraUniforms(this.idPhormShader, this.camera!.getId())
         Asset.Shader.setCameraUniforms(
             this.idPhormAlphaShader,
@@ -242,24 +67,5 @@ export default class MainEntity extends Entity {
             this.idPhormWaterShader,
             this.camera!.getId()
         )
-
-        Scene.copyTransform(this.idPhysics, this.idStaticInstance)
-    }
-    __ui() {
-        // Nanovg.fillStyle(1, 1, 1)
-        // Nanovg.fillRect(0, 0, 300, 300)
-        // Nanovg.fillStyle(this.color.r, this.color.g, this.color.b)
-        // Nanovg.fillRect(0, 0, 50, 50)
-        // Nanovg.strokeStyle(1 - this.color.r, 1 - this.color.g, 1 - this.color.b)
-        // Nanovg.strokeRect(50, 50, 50, 50)
-        // Nanovg.strokeLine(50, 50, 100, 100)
-        // Nanovg.strokeCircle(150, 150, 50)
-        // Nanovg.fillCircle(200, 150, 50)
-        // Nanovg.setFont("sans", 24)
-        // Nanovg.drawText(100, 250, "TEST")
-    }
-    __imgui() {
-        ImGui.text("I am a text box")
-        this.color.fromArray(ImGui.colorPicker4("color", this.color.toArray()))
     }
 }
